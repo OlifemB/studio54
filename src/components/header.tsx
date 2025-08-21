d5b11b4b5e04ebde6df84b8bc3f7aa0ca65286b4
f.rubcov <olifemtsx@gmail.com>
2025-08-21 14:12:23 +0300

    header colors

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -21,18 +21,20 @@ const nav_data = [
 
 export const Header = () => {
   return (
-    <header className={'h-[90px] border border-b border-[#D6D9DC]'}>
+    <header className={'h-[90px] border border-b border-[#D6D9DC] text-[#020202]'}>
       <div className={'container mx-auto flex flex-row h-full items-center justify-between'}>
         <Link href={'/'} className={'text-[36px]'}>architecture</Link>
 
         <nav className={'flex flex-row gap-[24px]'}>
           <ul className={'flex flex-row gap-[35px] text-[16px]'}>
             {nav_data.map((item) => (
-              <li key={item.title}>
+              <li key={item.title} className={'hover:text-[#2969CA] hover:underline duration-300'}>
                 <Link href={item.link}>{item.title}</Link>
               </li>
             ))}
           </ul>
+
+         <span className={'text-[#9C9393]'}>|</span>
 
           <div className={'text-[16px]'}>
             <Link href={'tel:89118183410'}>+7 (911) 818-34-10</Link>
